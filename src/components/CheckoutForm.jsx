83a440206308d141caff6c7db20188c01b4ca373
ErMuthii <er.muthii@gmail.com>
2024-07-18 00:42:50 +0300

    Updated checkout form

--- src/components/CheckoutForm.jsx
+++ src/components/CheckoutForm.jsx
@@ -1,5 +1,7 @@
+
 import React from 'react';
 import { CardElement, useStripe, useElements } from '@stripe/react-stripe-js';
+import { Card, Button, Spacer } from '@nextui-org/react';
 
 const CheckoutForm = () => {
   const stripe = useStripe();
@@ -28,12 +30,35 @@ const CheckoutForm = () => {
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <CardElement />
-      <button type="submit" disabled={!stripe}>
-        Pay
-      </button>
-    </form>
+    <div className="flex justify-center items-center mt-4">{/* Add pt-20 for top padding */}
+      <Card className="w-full max-w-md p-6 bg-white rounded-md shadow-md mx-auto"> {/* Add mx-auto to center the card */}
+        <form onSubmit={handleSubmit}>
+          <div className="mb-4">
+            <CardElement
+              options={{
+                style: {
+                  base: {
+                    fontSize: '16px',
+                    color: '#424770',
+                    '::placeholder': {
+                      color: '#aab7c4',
+                    },
+                  },
+                  invalid: {
+                    color: '#9e2146',
+                  },
+                },
+              }}
+              className="p-2 border border-gray-300 rounded"
+            />
+          </div>
+          <Spacer y={1.5} />
+          <Button type="submit" disabled={!stripe}  variant='ghost' color='primary' className="w-full" >
+             Pay
+          </Button>
+        </form>
+      </Card>
+    </div>
   );
 };
 
